2d91e1ff41a070b44103519c02a8ad215863398c
Augusto Lopes <meleu.dev@gmail.com>
2024-05-03 21:13:02 -0300

    first implementation

--- lib/tiozao.rb
+++ lib/tiozao.rb
@@ -1,8 +1,32 @@
 # frozen_string_literal: true
 
+require "httparty"
+
 require_relative "tiozao/version"
 
 module Tiozao
   class Error < StandardError; end
-  # Your code goes here...
+
+  # Get jokes from icanhazdadjoke.com
+  class Joke
+    @url = "https://icanhazdadjoke.com"
+    @headers = {
+      "Accept": "application/json",
+      "User-Agent": "Piadas de Tiozão"
+    }
+
+    def self.random
+      HTTParty.get(@url, headers: @headers)["joke"]
+    end
+
+    def self.search(term, limit = 3)
+      query = { term: term, limit: limit }
+      response = HTTParty.get(
+        "#{@url}/search",
+        headers: @headers,
+        query: query
+      )
+      response["results"].map { |result| result["joke"] }
+    end
+  end
 end
--- test/test_tiozao.rb
+++ test/test_tiozao.rb
@@ -7,7 +7,52 @@ class TestTiozao < Minitest::Test
     refute_nil ::Tiozao::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_random
+    stubbed_response = {
+      "id" => "R7UfaahVfFd",
+      "joke" => "a dad joke",
+      "status" => 200
+    }
+    expected_joke = "a dad joke"
+
+    HTTParty.stub :get, stubbed_response do
+      assert_equal expected_joke, Tiozao::Joke.random
+    end
+  end
+
+  def test_search
+    stubbed_response = {
+      "current_page" => 1,
+      "limit" => 20,
+      "next_page" => 2,
+      "previous_page" => 1,
+      "results" => [
+        {
+          "id" => "M7wPC5wPKBd",
+          "joke" => "joke 1"
+        },
+        {
+          "id" => "MRZ0LJtHQCd",
+          "joke" => "joke 2"
+        },
+        {
+          "id" => "usrcaMuszd",
+          "joke" => "joke 3"
+        }
+      ],
+      "search_term" => "",
+      "status" => 200,
+      "total_jokes" => 307,
+      "total_pages" => 15
+    }
+    expected_jokes = [
+      "joke 1",
+      "joke 2",
+      "joke 3"
+    ]
+
+    HTTParty.stub :get, stubbed_response do
+      assert_equal expected_jokes, Tiozao::Joke.search("joke")
+    end
   end
 end
